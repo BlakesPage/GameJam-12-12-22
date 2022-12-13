ab5da280b71aa513a5d7ffa7f819930c1cfb2e8d
BlakesPage <71866323+BlakesPage@users.noreply.github.com>
2022-12-13 15:43:08 +1030

    save

--- GameJam gameDev/Assets/Explosion.cs
+++ GameJam gameDev/Assets/Explosion.cs
@@ -7,7 +7,7 @@ public class Explosion : MonoBehaviour
     [SerializeField] private GameObject player;
     private void Awake()
     {
-        //player = GameObject.FindGameObjectWithTag("Player");
+        
     }
     private void Start()
     {
--- GameJam gameDev/Assets/GameButtons.cs
+++ GameJam gameDev/Assets/GameButtons.cs
@@ -6,6 +6,6 @@ public class GameButtons : MonoBehaviour
 {
     public void Restart()
     {
-        
+        // reload Scene
     }
 }
