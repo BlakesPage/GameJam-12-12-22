5fc35f87bfcc3f97d6a043d8e5b00dead1a9f31d
BlakesPage <71866323+BlakesPage@users.noreply.github.com>
2022-12-13 15:21:16 +1030

    base dev ui done

---
+++ GameJam gameDev/Assets/UiInfo.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiInfo : MonoBehaviour
+{
+    public GameObject playerUI;
+    public GameObject enemyUI;
+    public GameObject enemyspawnerUI;
+    public GameObject GunsUI;
+    public GameObject GlobalUI;
+
+    public void PlayerUI()
+    {
+        playerUI.SetActive(true);
+        enemyUI.SetActive(false);
+        enemyspawnerUI.SetActive(false);
+        GunsUI.SetActive(false);
+        GlobalUI.SetActive(false);
+    }
+
+    public void EnemyUI()
+    {
+        playerUI.SetActive(false);
+        enemyUI.SetActive(true);
+        enemyspawnerUI.SetActive(false);
+        GunsUI.SetActive(false);
+        GlobalUI.SetActive(false);
+    }
+    public void EnemySpawnerUI()
+    {
+        playerUI.SetActive(false);
+        enemyUI.SetActive(false);
+        enemyspawnerUI.SetActive(true);
+        GunsUI.SetActive(false);
+        GlobalUI.SetActive(false);
+    }
+    public void GunInfoUI()
+    {
+        playerUI.SetActive(false);
+        enemyUI.SetActive(false);
+        enemyspawnerUI.SetActive(false);
+        GunsUI.SetActive(true);
+        GlobalUI.SetActive(false);
+    }
+    public void GlobalInfoUI()
+    {
+        playerUI.SetActive(false);
+        enemyUI.SetActive(false);
+        enemyspawnerUI.SetActive(false);
+        GunsUI.SetActive(false);
+        GlobalUI.SetActive(true);
+    }
+}
